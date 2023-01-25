1d8bc966cf782a4b93ea94b38fad4097e2ad6b4a
karimamaaoui <maaouika07@gmail.com>
2023-01-25 21:46:43 +0100

    first commit

---
+++ Main/AdminDashboard.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author user
+ */
+public class AdminDashboard extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+      Parent root = FXMLLoader.load(getClass().getResource("../Views/Dashboard.fxml"));
+        primaryStage.setTitle("Admin Dashboard");
+        primaryStage.setScene(new Scene(root,1315,890));
+        primaryStage.show();}
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
