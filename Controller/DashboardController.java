2930eb8a9fb85081ad5967c7a57fe8dbe52d21f1
karimamaaoui <maaouika07@gmail.com>
2023-02-04 21:05:23 +0100

    first commit

--- Controller/DashboardController.java
+++ Controller/DashboardController.java
@@ -6,8 +6,15 @@
 package Controller;
 
 import java.net.URL;
+import java.util.Observable;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 
 /**
  * FXML Controller class
@@ -16,12 +23,39 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+     private PieChart pileChart;
+    @FXML
+    private LineChart<?, ?> lineChart;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        inLineChart();
+        iniPieChart();
     }    
     
+    
+    private void inLineChart(){
+        XYChart.Series series=new XYChart.Series();
+        series.getData().add(new XYChart.Data("Monday",8));
+        series.getData().add(new XYChart.Data("Tuesdat",12));
+        series.getData().add(new XYChart.Data("Wednesday",10));
+        series.getData().add(new XYChart.Data("Thursday",15));
+        series.getData().add(new XYChart.Data("Friday",12));
+        series.getData().add(new XYChart.Data("Saturday",5));
+        lineChart.getData().addAll(series);
+        lineChart.lookup(".chart-plot-background").setStyle(".-fx-background-color:transparent;");
+        series.getNode().setStyle("-fx-stroke: #FFD6DC");
+        
+    }
+    
+    private void iniPieChart(){
+        ObservableList <PieChart.Data> pieChartData =FXCollections.observableArrayList(
+        new PieChart.Data("Android", 15),
+        new PieChart.Data("JavaFX", 38),
+        new PieChart.Data("Java Swing", 18),
+        new PieChart.Data("React Native", 2)
+        );
+        pileChart.setData(pieChartData);
+    }
 }
